63b8ece74d2c9577e04b358e527a4b7155e0367f
Ronald Mannak <ronaldmannak@me.com>
2024-04-01 09:49:09 -0700

    swift lint change

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
 let package = Package(
     name: "mlx-libraries",
     platforms: [.macOS(.v14), .iOS(.v16)],
