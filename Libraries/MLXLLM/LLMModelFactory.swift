f18213d4ee6acac4863297cbc6a3aca51e557717
David Koski <dkoski@apple.com>
2024-12-10 10:26:24 -0800

    workaround for #150 -- important as this model is out new default

--- Libraries/MLXLLM/LLMModelFactory.swift
+++ Libraries/MLXLLM/LLMModelFactory.swift
@@ -221,9 +221,20 @@ private struct LLMUserInputProcessor: UserInputProcessor {
     }
 
     func prepare(input: UserInput) throws -> LMInput {
-        let messages = input.prompt.asMessages()
-        let promptTokens = try tokenizer.applyChatTemplate(messages: messages)
-        return LMInput(tokens: MLXArray(promptTokens))
+        do {
+            let messages = input.prompt.asMessages()
+            let promptTokens = try tokenizer.applyChatTemplate(messages: messages)
+            return LMInput(tokens: MLXArray(promptTokens))
+        } catch {
+            // #150 -- it might be a TokenizerError.chatTemplate("No chat template was specified")
+            // but that is not public so just fall back to text
+            let prompt = input.prompt
+                .asMessages()
+                .compactMap { $0["content"] }
+                .joined(separator: ". ")
+            let promptTokens = tokenizer.encode(text: prompt)
+            return LMInput(tokens: MLXArray(promptTokens))
+        }
     }
 }
 
