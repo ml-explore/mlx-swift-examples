7813a922f354f4fe45cd2a7b99605fc2e020904f
David Koski <46639364+davidkoski@users.noreply.github.com>
2024-12-17 08:17:53 -0800

    turn on docs build (#166)
    * turn on docs build
    
    * add hook in Package

--- Package.swift
+++ Package.swift
@@ -139,3 +139,12 @@ let package = Package(
         ),
     ]
 )
+
+if Context.environment["MLX_SWIFT_BUILD_DOC"] == "1"
+    || Context.environment["SPI_GENERATE_DOCS"] == "1"
+{
+    // docc builder
+    package.dependencies.append(
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
+    )
+}
