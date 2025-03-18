af062bd188f4cebf4c6a614f6e25d13b2f0de7e8
Anthony DePasquale <anthony@depasquale.org>
2025-03-18 18:39:08 +0100

    Format

--- Libraries/MLXLMCommon/Models/Gemma.swift
+++ Libraries/MLXLMCommon/Models/Gemma.swift
@@ -11,19 +11,19 @@ import MLXFast
 import MLXNN
 
 public enum Gemma {
-  /// Specialized norm for gemma
-  public class RMSNorm: Module, UnaryLayer {
-    let weight: MLXArray
-    let eps: Float
+    /// Specialized norm for gemma
+    public class RMSNorm: Module, UnaryLayer {
+        let weight: MLXArray
+        let eps: Float
 
-    public init(dimensions: Int, eps: Float = 1e-5) {
-      self.weight = MLXArray.ones([dimensions])
-      self.eps = eps
-      super.init()
-    }
+        public init(dimensions: Int, eps: Float = 1e-5) {
+            self.weight = MLXArray.ones([dimensions])
+            self.eps = eps
+            super.init()
+        }
 
-    public func callAsFunction(_ x: MLXArray) -> MLXArray {
-      return MLXFast.rmsNorm(x, weight: 1.0 + self.weight, eps: self.eps)
+        public func callAsFunction(_ x: MLXArray) -> MLXArray {
+            return MLXFast.rmsNorm(x, weight: 1.0 + self.weight, eps: self.eps)
+        }
     }
-  }
 }
