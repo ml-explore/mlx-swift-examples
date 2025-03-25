7530451165b789feb2a08c423d092187092d3f6f
Daniel Nata Nugraha <daniel.nata@flower.ai>
2025-03-25 10:12:10 +0000

    Add Deepseek V3 model

--- Libraries/MLXLLM/LLMModelFactory.swift
+++ Libraries/MLXLLM/LLMModelFactory.swift
@@ -40,6 +40,7 @@ public class LLMTypeRegistry: ModelTypeRegistry, @unchecked Sendable {
             "cohere": create(CohereConfiguration.self, CohereModel.init),
             "openelm": create(OpenElmConfiguration.self, OpenELMModel.init),
             "internlm2": create(InternLM2Configuration.self, InternLM2Model.init),
+            "deepseek_v3": create(DeepseekV3Configuration.self, DeepseekV3Model.init),
         ]
     }
 
@@ -165,6 +166,11 @@ public class LLMRegistry: AbstractModelRegistry, @unchecked Sendable {
         id: "mlx-community/Llama-3.2-3B-Instruct-4bit",
         defaultPrompt: "What is the difference between a fruit and a vegetable?"
     )
+  
+    static public let deepseek_r1_4bit = ModelConfiguration(
+        id: "mlx-community/DeepSeek-R1-4bit",
+        defaultPrompt: "Tell me about the history of Spain."
+    )
 
     private static func all() -> [ModelConfiguration] {
         [
@@ -187,6 +193,7 @@ public class LLMRegistry: AbstractModelRegistry, @unchecked Sendable {
             qwen2_5_7b,
             qwen2_5_1_5b,
             smolLM_135M_4bit,
+            deepseek_r1_4bit,
         ]
     }
 
