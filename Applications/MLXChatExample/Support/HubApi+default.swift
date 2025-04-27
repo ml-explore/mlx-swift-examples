8be2e44e6a76446c84eb4af80a50a8b7c6738cd3
Rudrank Riyam <rudrankriyam@gmail.com>
2025-04-27 20:27:27 +0530

    Add conditional default value for HubApi

--- Applications/MLXChatExample/Support/HubApi+default.swift
+++ Applications/MLXChatExample/Support/HubApi+default.swift
@@ -12,6 +12,10 @@ import Foundation
 extension HubApi {
     /// Default HubApi instance configured to download models to the user's Downloads directory
     /// under a 'huggingface' subdirectory.
+#if(os(macOS))
     static let `default` = HubApi(
         downloadBase: URL.downloadsDirectory.appending(path: "huggingface"))
+#else
+    static let `default` = HubApi()
+#endif
 }
