ee262fdd0670c598c0c8f389e45e5bd317c905aa
David Koski <dkoski@apple.com>
2025-05-01 08:21:35 -0700

    swift-format

--- Applications/MLXChatExample/Support/HubApi+default.swift
+++ Applications/MLXChatExample/Support/HubApi+default.swift
@@ -12,13 +12,13 @@ import Foundation
 extension HubApi {
     /// Default HubApi instance configured to download models to the user's Downloads directory
     /// under a 'huggingface' subdirectory.
-#if os(macOS)
-    static let `default` = HubApi(
-        downloadBase: URL.downloadsDirectory.appending(path: "huggingface")
-    )
-#else
-    static let `default` = HubApi(
-        downloadBase: URL.cachesDirectory.appending(path: "huggingface")
-    )
-#endif
+    #if os(macOS)
+        static let `default` = HubApi(
+            downloadBase: URL.downloadsDirectory.appending(path: "huggingface")
+        )
+    #else
+        static let `default` = HubApi(
+            downloadBase: URL.cachesDirectory.appending(path: "huggingface")
+        )
+    #endif
 }
