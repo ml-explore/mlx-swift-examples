f390d9e934ee42936c9a53927924e68666f340db
İbrahim Çetin <cetinibrahim.ci@gmail.com>
2025-05-04 11:42:02 +0300

    Add ToolTests

---
+++ Tests/MLXLMTests/ToolTests.swift
@@ -0,0 +1,65 @@
+import Foundation
+import MLXLMCommon
+import Testing
+
+struct WeatherInput: Codable {
+    let location: String
+    let unit: String
+}
+
+struct WeatherOutput: Codable {
+    let temperature: Double
+    let conditions: String
+}
+
+struct ToolSchemaTests {
+    @Test("Test Weather Tool Schema Generation")
+    func testWeatherToolSchemaGeneration() throws {
+        let tool = Tool<WeatherInput, WeatherOutput>(
+            name: "get_current_weather",
+            description: "Get the current weather in a given location",
+            parameters: [
+                .required(
+                    "location", type: .string, description: "The city, e.g. Istanbul"
+                ),
+                .optional(
+                    "unit",
+                    type: .string,
+                    description: "The type of the temperature unit",
+                    extraProperties: [
+                        "enum": ["celsius", "fahrenheit"]
+                    ]
+                ),
+            ]
+        ) { input in
+            WeatherOutput(temperature: 14.0, conditions: "Sunny")
+        }
+
+        let actual = tool.schema as NSDictionary
+
+        let expected: NSDictionary = [
+            "type": "function",
+            "function": [
+                "name": "get_current_weather",
+                "description": "Get the current weather in a given location",
+                "parameters": [
+                    "type": "object",
+                    "properties": [
+                        "location": [
+                            "type": "string",
+                            "description": "The city, e.g. Istanbul",
+                        ],
+                        "unit": [
+                            "type": "string",
+                            "description": "The type of the temperature unit",
+                            "enum": ["celsius", "fahrenheit"],
+                        ],
+                    ],
+                    "required": ["location"],
+                ],
+            ],
+        ]
+
+        #expect(actual == expected)
+    }
+}
