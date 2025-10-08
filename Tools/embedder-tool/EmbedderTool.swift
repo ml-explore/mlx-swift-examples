c485c12b91f8ae5e2105171730da45a3625e824f
Rudrank Riyam <rudrankriyam@gmail.com>
2025-10-09 03:48:54 +0530

    Add EmbedderTool command line utility

---
+++ Tools/embedder-tool/EmbedderTool.swift
@@ -0,0 +1,16 @@
+// Copyright © 2025 Apple Inc.
+
+import ArgumentParser
+import MLXEmbedders
+
+@main
+struct EmbedderTool: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Command line tool for working with MLX embedders"
+    )
+
+    mutating func run() async throws {
+        // Implementation will be expanded in following steps.
+    }
+}
+
