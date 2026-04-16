3735a8607f74f2aa8fabdcb9ad9d2ddd6932dae5
David Koski <dkoski@apple.com>
2026-04-16 11:14:35 -0700

    remove dtype -- was related to experiment in mlx-swift

--- Tools/Tutorial/Tutorial.swift
+++ Tools/Tutorial/Tutorial.swift
@@ -50,7 +50,7 @@ struct Tutorial {
         print(x[1])
 
         // make an array of shape [2, 2] filled with ones
-        let y = MLXArray.ones([2, 2], type: Float.self)
+        let y = MLXArray.ones([2, 2])
 
         // pointwise add x and y
         let z = x + y
